53c9968972944b910810922d79b78837d289a7eb
JD Crouch <jdcrouch5@gmail.com>
2015-12-31 17:19:25 +0900

    Set up flash messages for devise

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151231052508) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
